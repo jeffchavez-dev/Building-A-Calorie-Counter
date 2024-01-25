72b370b53d9ba777467e3a5b52dcc08704969c81
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-25 21:06:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,5 +31,6 @@ function isInvalidInput(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length
+    const HTMLString = ``
 
 }
