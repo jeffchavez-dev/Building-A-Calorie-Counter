4c2863424daf9325e98d4236bb2fce45b0ec412c
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-26 22:13:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,11 +38,11 @@ function addEntry() {
     //     <input type="number" placeholder="Calories" min="0" id="${entryDropdown.value}-${entryNumber}-calories">
     // `
     targetInputContainer.innerHTML += `
-    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
-    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-    <input type="number" placeholder="Calories" min="0" id="${entryDropdown.value}-${entryNumber}-calories">
-`
+        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+        <input type="number" placeholder="Calories" min="0" id="${entryDropdown.value}-${entryNumber}-calories">
+    `
 }
 
 addEntryButton.addEventListener('click', addEntry)
