0bce42d6eef4b968288d5f72d962b877e744e494
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-26 22:12:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,12 @@ function isInvalidInput(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1
-    const HTMLString = `
-        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-        <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
-        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-        <input type="number" placeholder="Calories" min="0" id="${entryDropdown.value}-${entryNumber}-calories">
-
-    `
+    // const HTMLString = `
+    //     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    //     <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+    //     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    //     <input type="number" placeholder="Calories" min="0" id="${entryDropdown.value}-${entryNumber}-calories">
+    // `
     targetInputContainer.innerHTML += HTMLString
 }
 
