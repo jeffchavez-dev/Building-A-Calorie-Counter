0c1e5ec90e04b82e0b101d3f20799f1fd96679b0
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-27 21:27:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -73,4 +73,5 @@ function calculateCalories(e) {
     const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]')
     const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]')
     const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]')
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs)
 }
