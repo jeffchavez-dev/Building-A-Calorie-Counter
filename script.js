c99de4e5435c2ebbccdb1576d7a46c1b69cf05b9
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-29 22:22:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -91,5 +91,7 @@ function calculateCalories(e) {
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories
     const surplusOrDeficit = remainingCalories >= 0 ? "Surplus" : "Deficit" 
 
-    output.innerHTML = ``
+    output.innerHTML = `
+        <span class="${surplusOrDeficit.toLowerCase()}"></span>
+    `
 }
